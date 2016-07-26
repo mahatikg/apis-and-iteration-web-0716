a6db16b577c32200738535ad09d3b1301553c3d5
Mahati Gollamudi <github email address>
2016-07-26 16:25:49 -0400

    Complete API Iteration Lab

--- bin/run.rb
+++ bin/run.rb
@@ -6,3 +6,6 @@ require_relative "../lib/command_line_interface.rb"
 welcome
 character = get_character_from_user
 show_character_movies(character)
+
+# films_hash = get_character_movies_from_api(character)
+# parse_character_movies(films_hash)
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,22 +1,43 @@
 require 'rest-client'
+require 'pry'
+require 'JSON'
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
+  all_the_characters = RestClient.get('http://www.swapi.co/api/people/')
+  character_hash = JSON.parse(all_the_characters)
   # iterate ove the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
-  # return value of this method should be collection of info about each film. 
+  # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list 
+  #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+  urls_of_films = []
+  character_hash["results"].each do |character_profile|
+    if character_profile["name"].downcase == character
+      urls_of_films = character_profile["films"]  # returns and array of strings
+    end
+  end
+  film_array = []
+  urls_of_films.each do |film_url|
+    film = RestClient.get(film_url)
+    film_array << JSON.parse(film)
+
+  end
+  #print film_array
+  film_array
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  list_of_movies =[]
+  films_hash.each do |movie_hash|
+    list_of_movies << movie_hash["title"]
+  end
+  print list_of_movies
 end
 
 def show_character_movies(character)
